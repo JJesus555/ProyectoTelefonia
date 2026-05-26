2b83b2bd83b132c417f5bbcd9631aade62951e3c
Jose Jesus <s21120206@alumnos.itsur.edu.mx>
2026-05-26 13:40:45 -0600

    Implementacion de permisos de telefonia y mensageria para pedirlos al usuario

--- TelefoniaAPP/app/src/main/java/com/example/telefoniaapp/MainActivity.kt
+++ TelefoniaAPP/app/src/main/java/com/example/telefoniaapp/MainActivity.kt
@@ -12,11 +12,26 @@ class MainActivity : ComponentActivity() {
     //Inicia y solicita los permisos que se necesitan y carga la interfaz principal
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        //requestPermissions()
+        requestPermissions()
 
         setContent {
             AppScreen(this)
+        }
+    }
+
+    // Es para solicitar los permisos para llamadas y enviar mensajes
+    // si toavia no se otorgan
+    private fun requestPermissions() {
+        val permisos = arrayOf(
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.READ_CALL_LOG
+        )
+
+        permisos.forEach {
+            if (ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(permisos, 1)
+            }
         }
     }
 }
